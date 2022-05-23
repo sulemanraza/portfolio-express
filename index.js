020cb27363371f322b2acce71bfbf07f7120d8c3
Suleman Raza <47869006+sulemanraza@users.noreply.github.com>
2022-05-23 17:55:57 +0200

    updata -path

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static('static'))
+app.use(express.static(path.join(__dirname, "static")));
 app.use(require("./routers/web.js"));
-
+console.log(path.join(__dirname, "static"))
 app.listen(PORT, () =>
   console.log(`Server listen on http://localhost:${PORT}`)
 );
